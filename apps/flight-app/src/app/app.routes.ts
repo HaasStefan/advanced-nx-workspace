edbdd82bb945e10e4c435b2fc6fb5be8932bd159
Stefan Haas <stefan.haas.privat@gmail.com>
2022-09-22 16:04:58 +0200

    frankenstein

--- apps/flight-app/src/app/app.routes.ts
+++ apps/flight-app/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { BasketComponent } from './basket/basket.component';
 import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import {
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 
 export const APP_ROUTES: Routes = [
   {
@@ -27,6 +31,19 @@ export const APP_ROUTES: Routes = [
         exposedModule: './Module',
       }).then((m) => m.PassengerModule),
   },
+
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry:
+        'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element', // <react-element>
+    } as WebComponentWrapperOptions,
+  },
+
   {
     path: '**',
     redirectTo: 'home',
