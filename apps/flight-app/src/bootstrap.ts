516e8a4239ecf7e60fc508ac814bab81c9671af8
Stefan Haas <stefan.haas.privat@gmail.com>
2022-09-22 16:17:27 +0200

    frankenstein bootstrap

--- apps/flight-app/src/bootstrap.ts
+++ apps/flight-app/src/bootstrap.ts
@@ -1,14 +1,11 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular-architects/module-federation-tools';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+bootstrap(AppModule, {
+  production: environment.production,
+  appType: 'shell'
+});
 
 
 
