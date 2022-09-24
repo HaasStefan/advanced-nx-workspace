36bdabf9f9de665b6ee431a860133fce41b22fc3
Stefan Haas <stefan.haas.privat@gmail.com>
2022-09-24 17:34:58 +0200

    ngrx + ddd with @angular-architects/ddd --ngrx

---
+++ libs/checkin/domain/src/index.ts
@@ -0,0 +1,8 @@
+export * from './lib/checkin-domain.module';
+
+export * from './lib/entities/passenger';
+export * from './lib/infrastructure/passenger.data.service';
+
+export * from './lib/+state/passenger/passenger.actions';
+
+export * from './lib/application/manage.facade';
---
+++ libs/checkin/domain/src/lib/application/manage.facade.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+
+import { loadPassenger } from '../+state/passenger/passenger.actions';
+import * as fromPassenger from '../+state/passenger/passenger.reducer';
+import * as PassengerSelectors from '../+state/passenger/passenger.selectors';
+import { Passenger } from '../entities/passenger';
+import { PassengerDataService } from '../infrastructure/passenger.data.service';
+
+@Injectable({ providedIn: 'root' })
+export class ManageFacade {
+  loaded$ = this.store.pipe(select(PassengerSelectors.getPassengerLoaded));
+  passengerList$ = this.store.pipe(select(PassengerSelectors.getAllPassenger));
+  selectedPassenger$ = this.store.pipe(select(PassengerSelectors.getSelected));
+
+  constructor(private store: Store<fromPassenger.PassengerPartialState>) {}
+
+  load(): void {
+    this.store.dispatch(loadPassenger());
+  }
+}
+
+//Alternative when not doing NGRX from the beginning:
+@Injectable({ providedIn: 'root' })
+export class AlternativeManageFacade {
+  private readonly _passengerList = new BehaviorSubject<Passenger[]>([]);
+  readonly passengerList$ = this._passengerList.asObservable();
+
+  constructor(private passengerService: PassengerDataService) {}
+
+  load(): void {
+    this.passengerService
+      .load()
+      .subscribe((passengers) => this._passengerList.next(passengers));
+  }
+}
