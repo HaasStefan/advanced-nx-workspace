b4423506af3a5085226d8972b07202bdfda244f7
Stefan Haas <stefan.haas.privat@gmail.com>
2022-09-26 14:13:11 +0200

    ddd

--- libs/flight-lib/src/lib/services/flight.service.ts
+++ libs/flight-lib/src/lib/services/flight.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs';
 import { Flight } from '../models/flight';
 
